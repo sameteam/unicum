c6f1648c2a52b396295e462f83c20c66a3b3b663
sullof <sullog@gmail.com>
2014-01-05 16:49:11 -0800

    Fixed bug with generate and getTime

--- test/test.js
+++ test/test.js
@@ -1,10 +1,8 @@
 
 unicum = require("../lib/unicum").init(require('../conf/unicum-config').config);
 
-console.log(JSON.stringify(require('../conf/unicum-config').config,null,4));
-//unicum.genKey(1,function (val) {
-//	
-//
-//console.log(val);
-//console.log(unicum.getTime(val), unicum._epoch);
-//});
+//console.log(JSON.stringify(require('../conf/unicum-config').config,null,4));
+unicum.generate(1,function (val) {
+	console.log(val);
+	console.log(unicum.getTime(val), unicum._epoch);
+});
